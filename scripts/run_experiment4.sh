5a509f980dc005ac8813a5b66877f07e6fe9822b
Liz Tan <elizabethsztan@gmail.com>
2025-12-15 15:21:07 +0000

    add more experiments

--- scripts/run_experiment4.sh
+++ scripts/run_experiment4.sh
@@ -34,7 +34,7 @@ source /cephfs/store/gr-mc2473/eszt2/venv/bin/activate
 
 # Define parameter ranges
 PCA_COMPS_I=(16 32 64 128)
-PCA_COMPS_O=(32 64 128 256)
+PCA_COMPS_O=(8 16)
 MAX_CHARS=750000
 
 # Total number of combinations
